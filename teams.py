1b6a3dfb03b50956371f76f50f3f4e4d0f63de9b
dato813 <datunaroblox1@gmail.com>
2024-05-17 08:53:06 -0700

    Create teams.py

---
+++ teams.py
@@ -0,0 +1,11 @@
+team_1=input("write first team ")
+team_2=input("write second team ")
+# real madrid vs dortmud
+if "real madrid"==team_1 and "dortmud"==team_2:
+    print("real madrid has won 999999999999999999999999999999999-0")
+elif "jiventus"==team_1 and "bayern"==team_2:
+    print("jiventus has won 28793423489237592367982365283659_0")
+elif "paris"==team_1 and "real betis"==team_2:
+    print("real betis has won 9322354324324234323424238427384283947828794297842978423789478923489723978423987423987479823498723498723987423984982734892347823894723784278394-0")
+else:
+    print("incoretc teams")
