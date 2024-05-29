ea8f12ac82377486f28d67e75c7d3e2ac0e463c1
dato813 <datunaroblox1@gmail.com>
2024-05-29 08:51:33 -0700

    Update teams.py

--- teams.py
+++ teams.py
@@ -1,13 +1,12 @@
-team_1=input("write first team ")
-team_2=input("write second team ")
-# real madrid vs dortmud
-#I love real madrid
-if "real madrid"==team_1 and "dortmud"==team_2:
-    print("real madrid has won 999999999999999999999999999999999-0")
-elif "jiventus"==team_1 and "bayern"==team_2:
-    print("jiventus has won 28793423489237592367982365283659_0")
-elif "paris"==team_1 and "real betis"==team_2:
-    print("real betis has won 9322354324324234323424238427384283947828794297842978423789478923489723978423987423987479823498723498723987423984982734892347823894723784278394-0")
-else:
-    print("incoretc teams")
-#georgia is contri!!!!
+#jumperiko yemala123!zangibabua$
+
+haircolor=input("write ur hair color dark,blond,ginger,inchinhanchin")
+team=input("write ur team madrid,psg,barca,socherid")
+if haircolor=="dark"and team=="madrid":
+    print("yeamla123!")
+elif haircolor=="blond"and team=="psg":
+    print("jumperiko")
+elif haircolor=="ginger"and team=="barca":
+    print("zangibabua$")
+elif haircolor=="inchinhanchin"and team=="socherid":
+    print("yugsadjhvgjhvdsgdvfeghsdfhjevsjhfghsdfgveshgvfsehjfgsyhejfgyhjsefgihusedhuiasegyfujsrftguhsetgyhudfsghfsdgxhjdfghseedtgyuwedgwtgfyhufdrguyhfegwsydgyhsgydwegydw3tgy edrwgyrgywrytg")
